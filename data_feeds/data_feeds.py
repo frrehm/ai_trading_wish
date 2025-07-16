59694194ae7072d9b2ec1f6f0958d29a6e677bcb
frrehm <frrehm@yahoo.co.uk>
2025-07-16 08:36:55 +0100

    Update data_feeds.py

--- data_feeds/data_feeds.py
+++ data_feeds/data_feeds.py
@@ -10,16 +10,31 @@ def fetch_ism_data():
     return df
 
 def get_all_indicators():
+    # Load ISM PMI
     ism_df = fetch_ism_data()
-    ism = ism_df["ISM_PMI"]
+    ism = ism_df["ISM_PMI"].copy()
     ism.name = "ISM_PMI"
+    ism.index = pd.to_datetime(ism.index)
 
+    # Fetch FRED indicators
     umcsent = fetch_fred_series("UMCSENT")
-    umcsent.name = "UMCSI"
-
     housing = fetch_fred_series("HOUST")
+
+    # Ensure all have datetime index
+    umcsent.index = pd.to_datetime(umcsent.index)
+    housing.index = pd.to_datetime(housing.index)
+
+    # Rename
+    umcsent.name = "UMCSI"
     housing.name = "HousingStarts"
 
+    # Combine
     df = pd.concat([ism, umcsent, housing], axis=1)
+
+    # Make sure index is datetime and sorted
+    df.index = pd.to_datetime(df.index)
+    df = df.sort_index()
+
+    # Monthly average
     df = df.dropna().resample("M").mean()
     return df
